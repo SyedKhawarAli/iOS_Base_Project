6a6a601fcecd0308b84656889725452125a52846
Syed Khawar Ali Shah <khawarali5@gmail.com>
2022-04-25 17:35:29 +0500

    Project Added

---
+++ iOS_Base_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Base_Project
+//
+//  Created by EAPPLE on 25/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
